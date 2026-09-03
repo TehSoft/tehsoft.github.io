3754eb8d729e94816e0700132c9d9cc041856fa6
TehSoft <tehsoft@outlook.hu>
2026-09-03 20:55:31 +0200

    Add login functionality with hex code verification

---
+++ login/script.js
@@ -0,0 +1,26 @@
+function strToHexa(szoveg) {
+    let hex = '';
+    for (let i = 0; i < szoveg.length; i++) {
+        hex += szoveg.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+
+const adatbazis = {
+    "3230313330313131": "Horváth Sebestyén"
+};
+
+function bejelentkezes() {
+    const beirtKod = document.getElementById("kodInput").value;
+    const beirtHexa = strToHexa(beirtKod);
+
+    if (adatbazis.hasOwnProperty(beirtHexa)) {
+        const felhasznaloNeve = adatbazis[beirtHexa];
+        localStorage.setItem("bejelentkezettFelhasznalo", felhasznaloNeve);
+        document.getElementById("loginFormSzekcio").style.display = "none";
+        document.getElementById("adminPanel").style.display = "block";
+        document.getElementById("aktivUser").innerText = felhasznaloNeve;
+    } else {
+        alert("Hibás vagy ismeretlen kód!");
+    }
+}

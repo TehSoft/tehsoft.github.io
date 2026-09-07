13c74dd4add87fd8304c08a45d5c285d861f0a69
TehSoft <tehsoft@outlook.hu>
2026-09-07 13:39:56 +0200

    Update user data in adatbazis object

--- login/script.js
+++ login/script.js
@@ -7,8 +7,9 @@ function strToHexa(szoveg) {
 }
 
 const adatbazis = {
-    "3230313330313131": "Horváth Sebestyén",
-    "70617373776f726c64": "Cacaoway"
+    "3230313330313131": "HSebi",
+    "70617373776f726c64": "Cacaoway",
+    "61646d696e4031315F4c": "Radminl01"
 };
 
 function bejelentkezes() {
